3ce561781a9e8fa9426ccba8581848a699dba79f
Alexander Eckhart <eckhartalex@gmail.com>
2018-01-11 17:37:10 -0700

    Merge File and Rank

--- src/main/kotlin/org/alex34567/chess/Rank.kt
+++ src/main/kotlin/org/alex34567/chess/Rank.kt
@@ -1,9 +1,59 @@
 package org.alex34567.chess
 
-class Rank private constructor(private val rawRank: Int) : Comparable<Rank> {
+private fun <T> genPool(constructor: (Int) -> T): List<T> {
+    val pool: MutableList<T> = ArrayList(8)
 
+    for (x in 0..7) {
+        pool.add(constructor(x))
+    }
+
+    return pool
+}
+
+sealed class RankFileSuper<out T> {
+    abstract fun toInt(): Int
+    abstract fun newInstance(rankFile: Int): T?
+}
+
+sealed class RankFile<T : RankFileSuper<T>> constructor(protected val rawRankFile: Int) :
+        Comparable<T>, RankFileSuper<T>() {
+
+    abstract override fun toString(): String
+
+    abstract override fun equals(other: Any?): Boolean
+
+    override final fun hashCode(): Int {
+        return rawRankFile.hashCode()
+    }
+
+    operator fun plus(other: T): T? {
+        return other.newInstance(rawRankFile + other.toInt())
+    }
+
+    operator fun minus(other: T): T? {
+        return other.newInstance(rawRankFile - other.toInt())
+    }
+
+    override final operator fun compareTo(other: T): Int {
+        if (rawRankFile < other.toInt()) return -1
+
+        if (rawRankFile > other.toInt()) return 1
+
+        return 0
+    }
+
+    operator fun rangeTo(other: T): Iterable<T> {
+        //Handle backwards case
+        if (this >= other) return emptyList()
+
+        //Ignore null because other cannot be out of bounds
+        return (rawRankFile..other.toInt()).map({ newInstance(it)!! })
+    }
+}
+
+class Rank private constructor(rank: Int) : RankFile<Rank>(rank) {
     override fun toString(): String {
-        return Integer.toString(rawRank)
+        return Integer.toString(toInt())
     }
 
     override fun equals(other: Any?): Boolean {
@@ -11,46 +61,19 @@ class Rank private constructor(private val rawRank: Int) : Comparable<Rank> {
 
         if (other !is Rank) return false
 
-        if (rawRank == other.toInt()) return true
+        if (toInt() == other.toInt()) return true
 
         return false
     }
 
-    override fun hashCode(): Int {
-        return rawRank.hashCode()
+    override fun toInt(): Int {
+        return rawRankFile + 1
     }
 
-    operator fun plus(other: Rank): Rank? {
-        return newInstance(rawRank + other.toInt())
-    }
-
-    operator fun minus(other: Rank): Rank? {
-        return newInstance(rawRank - other.toInt())
-    }
-
-    override operator fun compareTo(other: Rank): Int {
-        if (rawRank < other.toInt()) return -1
-
-        if (rawRank > other.toInt()) return 1
-
-        return 0
-    }
-
-    operator fun rangeTo(other: Rank): Iterable<Rank> {
-        //Handle backwards case
-        if (this >= other) return emptyList()
-
-        //Ignore null because other cannot be out of bounds
-        return (rawRank..other.toInt()).map({ newInstance(it)!! })
-    }
-
-    fun toInt(): Int {
-        return rawRank
-    }
+    override fun newInstance(rankFile: Int): Rank? = Companion.newInstance(rankFile)
 
     companion object {
-
-        private val POOL = genPool()
+        private val POOL = genPool { rank -> Rank(rank) }
 
         val ONE get() = POOL[0]
         val TWO get() = POOL[1]
@@ -61,20 +84,52 @@ class Rank private constructor(private val rawRank: Int) : Comparable<Rank> {
         val SEVEN get() = POOL[6]
         val EIGHT get() = POOL[7]
 
-        private fun genPool(): List<Rank> {
-            val pool: MutableList<Rank> = ArrayList(8)
+        fun newInstance(rankFile: Int): Rank? {
+            if (rankFile < 1 || rankFile > 8) return null
 
-            for (x in 1..8) {
-                pool.add(Rank(x))
-            }
-
-            return pool
+            return POOL[rankFile - 1]
         }
 
-        fun newInstance(rank: Int): Rank? {
-            if (rank < 1 || rank > 8) return null
+    }
+}
 
-            return POOL[rank - 1]
+class File private constructor(file: Int) : RankFile<File>(file) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+
+        if (other !is File) return false
+
+        if (toInt() == other.toInt()) return true
+
+        return false
+    }
+
+    override fun toInt(): Int {
+        return rawRankFile
+    }
+
+    override fun newInstance(rankFile: Int): File? = Companion.newInstance(rankFile)
+
+    override fun toString(): String {
+        return ('A' + toInt()).toString()
+    }
+
+    companion object {
+        private val POOL = genPool { file -> File(file) }
+
+        val A get() = POOL[0]
+        val B get() = POOL[1]
+        val C get() = POOL[2]
+        val D get() = POOL[3]
+        val E get() = POOL[4]
+        val F get() = POOL[5]
+        val G get() = POOL[6]
+        val H get() = POOL[7]
+
+        fun newInstance(rankFile: Int): File? {
+            if (rankFile < 0 || rankFile > 7) return null
+
+            return POOL[rankFile]
         }
     }
 }
